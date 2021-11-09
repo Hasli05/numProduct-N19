0e1a8199a44b57e0a27d00f3f7acf6ce6cf81506
Max <msxkurko3@gmail.com>
2021-11-09 17:24:30 +0200

    V1

---
+++ numProduct/numProduct.cpp
@@ -0,0 +1,19 @@
+﻿using namespace std;
+#include <iostream>
+
+double numProduct(double numbers) {
+    double sum = 1;
+    for (int i = 1; i < numbers; ++i) {
+        sum = sum * (i + 1);
+    }
+    return sum;
+}
+
+int main()
+{
+    double rawNumbers = 0;
+    cout << "Enter value :" << endl;
+    cin >> rawNumbers;
+    
+    cout << numProduct(rawNumbers);
+}
